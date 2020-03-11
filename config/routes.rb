093b6a95a2230c737a204492174ed291353904f2
ricardo39985 <ricardo39985@gmail.com>
2020-03-11 17:04:54 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'products/index'
+
+  get 'products/add'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'application#homepage'
+  post 'cart', to: 'application#cart'
 end
